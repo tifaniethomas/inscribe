82206465bffbfaf088b5ff557c95871bca7fd4d9
Tifanie Thomas <tifanie.n.thomas@gmail.com>
2023-10-23 17:57:14 -0500

    Remove Title Model

--- models/textBlock.js
+++ models/textBlock.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const textBlockSchema = new Schema ({
-    title: {type: Schema.Types.ObjectId, ref: "Title", required: true},
     text: {type: String, required: true},
     position: {type: Number, required: true},
     user: {type: Schema.Types.ObjectId, ref: "User", required: true}
